f44a5ee2359a3b4a6e89a5c5308b530f8008def9
pavett <opergad@gmail.com>
2019-09-10 11:45:13 +0300

    Мои изменения проекта 10.09.19

---
+++ Pavlyuchenko Sergey/Pong/Pong/StringHelper.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<sstream>
+
+class StringHelper
+{
+public:
+	StringHelper() {};
+	~StringHelper() {};
+	static std::string int2str(int x) {
+		// converts int to string
+		std::stringstream ss;
+		ss << x;
+		return ss.str();
+	}
+};
+
