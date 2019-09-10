4f93d4bdb238b6705bc6e4ec3db352d920dc3abb
azureitstepkharkov <azure.itstep.kharkov@gmail.com>
2019-09-10 11:41:07 +0300

    Введено разбиение на классы - иллюстрация принципа Single Respon. коммент про патерн Singleton и механизм делегатов

--- Ivan Petrov/Pong/Pong/pch.h
+++ Ivan Petrov/Pong/Pong/pch.h
@@ -9,6 +9,15 @@
 #ifndef PCH_H
 #define PCH_H
 
-// TODO: add headers that you want to pre-compile here
+// a few keycodes from 
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx
+#define VK_W 0x57
+#define VK_S 0x53
+// window size and update rate (60 fps)
+const int width = 434;
+const int height = 200;
+const int interval = 1000 / 60;
+
+
 
 #endif //PCH_H
