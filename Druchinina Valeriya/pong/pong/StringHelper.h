3c3ccd0fa1e6144b564d4ed2f213f4c81a1c9d2c
vchumack <vchumack@glmedia.org>
2019-09-11 22:43:03 +0300

    Добавление проекта Pong и readme.txt

---
+++ Druchinina Valeriya/pong/pong/StringHelper.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream> 
+using namespace std;
+
+class StringHelper
+{
+public:
+	StringHelper() {};
+
+	~StringHelper() {};
+
+
+	static std::string int2str(int x)
+	{
+		// converts int to string
+		std::stringstream ss;
+		ss << x;
+		return ss.str();
+	}
+
+};
+
---
+++ Druchinina Valeriya/pong/pong/pch.cpp
@@ -0,0 +1,23 @@
+﻿// pch.cpp: source file corresponding to pre-compiled header; necessary for compilation to succeed
+
+#include "pch.h"
+#include "MathHelper.h"
+#include "StringHelper.h"
+
+// In general, ignore this file, but keep it around if you are using pre-compiled headers.
+// a few keycodes from 
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx
+#define VK_W 0x57
+#define VK_S 0x53
+	//presents
+// window size and update rate (60 fps)
+const int width = 434;
+const int height = 200;
+const int interval = 1000 / 60;
+//presents
+
+
+
+
+MathHelper mh;
+// #endif
