9fd2aca3a19a0aab6d1758721d27df22cf104b44
EvgeniyVanyan <mr.becker228@gmail.com>
2019-09-13 11:37:06 +0300

    Добавление папки проекта

---
+++ Exam/Работы_для_проверки/Evgeniy Vanyan/Adress/Adress/AnimalEntity.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "AnimalEntity.h"
+
+
+AnimalEntity::AnimalEntity(int sex, int age)
+{
+	this->sex = sex;
+	this->age = age;
+}
+
+AnimalEntity::~AnimalEntity()
+{
+}
---
+++ Exam/Работы_для_проверки/Evgeniy Vanyan/Adress/Adress/Rule1.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Rule1.h"
+
+
+Rule1::Rule1()
+{
+	this->hours = 0;
+	this->rate = 0;
+}
+Rule1::Rule1(double hours, double rate)
+{
+	this->hours = hours;
+	this->rate = rate;
+}
