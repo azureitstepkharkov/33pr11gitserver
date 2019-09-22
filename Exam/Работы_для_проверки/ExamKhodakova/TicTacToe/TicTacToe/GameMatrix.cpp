f73fbefe296619c4da7f898e3288f57c35a4b486
Vlad Khodakov <Vlad92X@gmail.com>
2019-09-22 17:15:27 +0300

    my work

---
+++ Exam/Работы_для_проверки/ExamKhodakova/TicTacToe/TicTacToe/GameMatrix.cpp
@@ -0,0 +1,83 @@
+#include "stdafx.h"
+#include "GameMatrix.h"
+
+
+void GameMatrix::startMatrix()
+{
+	for (int j = 0; j < this->sizeY; j++)
+	{
+		for (int i = 0; i < this->sizeX; i++)
+		{
+			this->matrix[j][i] = 2;
+
+		}
+
+	}
+
+}
+
+GameMatrix::GameMatrix()
+{
+	this->matrix = new int*[this->sizeY];
+	for (int j = 0; j < this->sizeY; j++)
+	{
+		this->matrix[j]= new int[this->sizeX];
+	}
+
+	this->startMatrix();
+}
+
+
+GameMatrix::~GameMatrix()
+{
+	for (int j = 0; j < this->sizeY; j++)
+	{
+		delete[] this->matrix[j];
+	}
+	delete[]this->matrix;
+}
+
+int ** GameMatrix::getBoardMatrix()
+{
+	return this->matrix;
+}
+
+int GameMatrix::get(int i, int j)
+{
+	return this->matrix[j][i];
+}
+
+void GameMatrix::set(int i, int j, int value)
+{
+	if (value <= 2 && value >= 0)
+	{
+		if(i >= 0 && i < this->sizeX && j >= 0 && j < this->sizeY)	this->matrix[j][i] = value;
+
+		else cout << "Вы вышли за диапазон!!!" << endl;
+	}
+	else cout << " Вы ввели не правильное значение! " << endl;
+}
+
+void GameMatrix::setX(int i, int j)
+{
+	if (i >= 0 && i < this->sizeX && j >= 0 && j < this->sizeY)	this->matrix[j][i] = 1;
+
+	else cout << "Вы вышли за диапазон!!!" << endl;
+}
+
+void GameMatrix::setO(int i, int j)
+{
+	if (i >= 0 && i < this->sizeX && j >= 0 && j < this->sizeY)	this->matrix[j][i] = 0;
+
+	else cout << "Вы вышли за диапазон!!!" << endl;
+}
+
+int GameMatrix::SizeX()
+{
+	return this->sizeX;
+}
+
+int GameMatrix::SizeY()
+{
+	return this->sizeY;
+}
